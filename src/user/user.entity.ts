3de5b797b9f76a663224fe70b77e0153f1bfdac1
Vineet <vineetgoyat1@gmail.com>
2026-07-30 23:56:09 +0530

    postgreSQL

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -4,8 +4,8 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 @Entity()
 export class User {
     @PrimaryGeneratedColumn()
-    id: number;
+    'id': number;
 
     @Column()
-    name: string;
+    'name': string;
 }
