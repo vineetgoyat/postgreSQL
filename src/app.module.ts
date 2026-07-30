1d87e1ef20fb76c571e7155cfa0e01fe89cd1f94
Vineet <vineetgoyat1@gmail.com>
2026-07-31 00:23:29 +0530

    postgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'postgres',}),
+      type: 'postgres',
       url: process.env.DATABASE_URL,
+    }),
     UserModule],
   controllers: [AppController],
   providers: [AppService],
