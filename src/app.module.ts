2bf2bc54cd6344995cc0cb77a2e04db9c6e26453
Vineet <vineetgoyat1@gmail.com>
2026-07-31 00:24:16 +0530

    postgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,7 @@ import { ConfigModule } from '@nestjs/config';
       type: 'postgres',
       url: process.env.DATABASE_URL,
       autoLoadEntities: true,
+      synchronize: true,
     }),
     UserModule],
   controllers: [AppController],
