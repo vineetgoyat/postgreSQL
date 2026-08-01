ed89435e697f23c2779819400877eba1781db8c4
Vineet <vineetgoyat1@gmail.com>
2026-08-02 01:12:26 +0530

    postgreSQL

--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Employee } from './employees.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class EmployeesService {
     constructor(
         @InjectRepository(Employee)
+        private employeeRepository : Repository<Employee>
     ) {}
 }
