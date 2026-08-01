c36fa2027c61de15e57095c409a5767ce47838b4
Vineet <vineetgoyat1@gmail.com>
2026-08-01 20:44:06 +0530

    postgreSQL

--- src/employees/employees.module.ts
+++ src/employees/employees.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { EmployeesService } from './employees.service';
 
-@Module({})
+@Module({
+  providers: [EmployeesService]
+})
 export class EmployeesModule {}
