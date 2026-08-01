de34fa8f4387fdf69719a1e93e5b160275b8b3d2
Vineet <vineetgoyat1@gmail.com>
2026-08-02 01:33:41 +0530

    postgreSQL

--- src/employees/employees.module.ts
+++ src/employees/employees.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { EmployeesController } from './employees.controller';
+import { Employee } from './employees.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports:[
+    TypeOrmModule.forFeature([Employee])
+  ],
   providers: [EmployeesService],
   controllers: [EmployeesController]
 })
