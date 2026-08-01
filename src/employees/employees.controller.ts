4a7092b101a067562aba0e18f8005397d4b1053e
Vineet <vineetgoyat1@gmail.com>
2026-08-02 01:28:37 +0530

    postgreSQL

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { EmployeesService } from './employees.service';
+import { Employee } from './employees.entity';
 
 @Controller('employees')
-export class EmployeesController {}
+export class EmployeesController {
+    constructor(private readonly employeesService: EmployeesService) {}
+
+    @Post()
+    async createEmployee(@Body() body: Partial<Employee>): Promise<Employee> {
+        return this.employeesService.create(body);
+    }
+
+}
