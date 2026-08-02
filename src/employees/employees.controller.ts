bec0609414c7292f5fb23bba9fec0d77407125e3
Vineet <vineetgoyat1@gmail.com>
2026-08-02 17:30:14 +0530

    postgreSQL

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { Employee } from './employees.entity';
 
@@ -9,6 +9,11 @@ export class EmployeesController {
     @Post()
     async createEmployee(@Body() body: Partial<Employee>): Promise<Employee> {
         return this.employeesService.create(body);
+    }
+
+    @Get()
+    async findAll(): Promise<Employee[]> {
+        return this.employeesService.findAll();
     }
 
 }
