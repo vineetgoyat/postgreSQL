a6b3af736af7fd46c1f3fe0773fcf532e90a04d2
Vineet <vineetgoyat1@gmail.com>
2026-08-05 18:13:38 +0530

    postgreSQL

--- src/auth/supabase-auth/supabase-auth.guard.ts
+++ src/auth/supabase-auth/supabase-auth.guard.ts
@@ -1,12 +1,16 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import * as jwt from 'jsonwebtoken';
+import { Request } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class SupabaseAuthGuard implements CanActivate {
+  constructor(private configService: ConfigService) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    
     return true;
   }
 }
