4302388681bc1a9a0db3eb3fa36bd338c11458e2
Vineet <vineetgoyat1@gmail.com>
2026-08-05 18:02:56 +0530

    postgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,9 @@ import { EmployeesModule } from './employees/employees.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DATABASE_URL,
