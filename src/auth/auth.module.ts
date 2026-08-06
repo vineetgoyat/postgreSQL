4ef41d41cbf4f7c6806313a0a2234e8e0c449070
Vineet <vineetgoyat1@gmail.com>
2026-08-07 00:54:38 +0530

    postgreSQL

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,12 +4,14 @@ import { AuthService } from './auth.service';
 import { User, UserSchema } from './user.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
     imports : [
         MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
         JwtModule.registerAsync({
-            imports: [],
+            imports: [ConfigModule],
+            inject: [ConfigService],
 
         })
     ],
