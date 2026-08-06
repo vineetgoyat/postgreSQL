247b3aefa955fe471bc5a5c903fea7e76d688ff6
Vineet <vineetgoyat1@gmail.com>
2026-08-07 02:11:19 +0530

    postgreSQL

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) {}
+
+    @Post('signup')
+    signUp(@Body() body: { email:string, password:string }) {
+        return this.authService.signUp(body.email, body.password);
+    }
 }
