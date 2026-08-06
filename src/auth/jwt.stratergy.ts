267c96740b7df826584e3ef1a112249d568c782b
Vineet <vineetgoyat1@gmail.com>
2026-08-07 01:31:34 +0530

    postgreSQL

--- src/auth/jwt.stratergy.ts
+++ src/auth/jwt.stratergy.ts
@@ -1,7 +1,9 @@
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
 import { ConfigService } from "@nestjs/config";
+import { Injectable } from "@nestjs/common";
 
+@Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
     constructor( private configService : ConfigService) {
         super({
@@ -9,5 +11,9 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
             secretOrKey: configService.get<string>('MY_JWT_SECRET')
 
         });
+    }
+
+    async validate(payload: any) {
+        return { userId: payload.sub, email: payload.email };
     }
 }
