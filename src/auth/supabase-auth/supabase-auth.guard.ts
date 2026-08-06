caeb5a5f8952ab9893f17f38eb9613477f2fca9b
Vineet <vineetgoyat1@gmail.com>
2026-08-06 21:09:27 +0530

    postgreSQL

--- src/auth/supabase-auth/supabase-auth.guard.ts
+++ src/auth/supabase-auth/supabase-auth.guard.ts
@@ -1,31 +1,29 @@
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
-import { Observable } from 'rxjs';
-import * as jwt from 'jsonwebtoken';
 import { Request } from 'express';
 import { ConfigService } from '@nestjs/config';
+import { jwtVerify, createRemoteJWKSet } from 'jose';
 
 @Injectable()
 export class SupabaseAuthGuard implements CanActivate {
   constructor(private configService: ConfigService) {}
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest<Request>();
     const authHeader = request.headers['authorization'];
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
       throw new UnauthorizedException('Missing or invalid Authorization header');
     }
     const token = authHeader.split(' ')[1];
-    const jwtSecret = this.configService.get<string>('SUPABASE_JWT_SECRET');
-    if(!jwtSecret) {
-      throw new UnauthorizedException('Missing JWT secret in configuration');
-    }
+
+    const projectUrl = this.configService.get<string>('SUPABASE_URL'); // e.g. https://bjgwskjgloggdufmqitw.supabase.co
+    const JWKS = createRemoteJWKSet(new URL(`${projectUrl}/auth/v1/.well-known/jwks.json`));
+
     try {
-      const decode = jwt.verify(token, jwtSecret);
-      request['user'] = decode;
+      const { payload } = await jwtVerify(token, JWKS);
+      request['user'] = payload;
       return true;
     } catch (error) {
-      console.error('JWT verify failed:', error instanceof Error ? error.name : 'Unknown', '-', error instanceof Error ? error.message : String(error));
+      console.error('JWT verify failed:', error instanceof Error ? error.message : error);
       throw new UnauthorizedException('Invalid or expired token');
     }
   }
