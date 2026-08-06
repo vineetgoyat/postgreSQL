7b6a9f3063e62d55a4330c4890a1bdbaef155bf3
Vineet <vineetgoyat1@gmail.com>
2026-08-07 02:53:45 +0530

    postgreSQL

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { User, UserSchema } from './user.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './jwt.stratergy';
 
 @Module({
     imports : [
@@ -20,6 +21,6 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         })
     ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
