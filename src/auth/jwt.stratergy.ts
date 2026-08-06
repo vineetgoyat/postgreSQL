2abc1b9e85b0e274c3a3e9df93eb5469611b1103
Vineet <vineetgoyat1@gmail.com>
2026-08-07 01:14:14 +0530

    postgreSQL

--- src/auth/jwt.stratergy.ts
+++ src/auth/jwt.stratergy.ts
@@ -1,5 +1,6 @@
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
+import { ConfigService } from "@nestjs/config";
 
 export class JwtStrategy extends PassportStrategy(Strategy) {
 
