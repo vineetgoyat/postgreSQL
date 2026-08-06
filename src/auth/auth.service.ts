33f597628998b35fb16e5eec1dd1c18fc01b1008
Vineet <vineetgoyat1@gmail.com>
2026-08-07 01:37:45 +0530

    postgreSQL

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,11 +2,12 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from './user.schema';
 import { Model } from 'mongoose';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
     constructor(
-         @InjectModel(User.name) private userModel: Model <UserDocument>
+         @InjectModel(User.name) private userModel: Model <UserDocument>, private jwtService: JwtService
     ){}
    
 }
