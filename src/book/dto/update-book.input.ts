600cb1496ada2050c43a4c4ecb6d6162a480679c
Vineet <vineetgoyat1@gmail.com>
2026-08-07 19:24:49 +0530

    postgreSQL graphql

---
+++ src/book/dto/update-book.input.ts
@@ -0,0 +1,3 @@
+import { CreateBookInput } from './create-book.input';
+import { InputType, Field, PartialType, ID } from '@nestjs/graphql';
+import { IsNotEmpty, IsString, IsOptional } from 'class-validator';
