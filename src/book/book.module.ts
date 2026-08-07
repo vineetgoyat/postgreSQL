abe1b480d7e8e0d3677b0f281621d3ea989e9295
Vineet <vineetgoyat1@gmail.com>
2026-08-07 19:05:17 +0530

    postgreSQL graphql

--- src/book/book.module.ts
+++ src/book/book.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BookService } from './book.service';
 import { BookResolver } from './resolvers/book.resolver';
+import { Book, BookSchema} from './model/book.model';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Book.name, schema: BookSchema }])
+  ],
   providers: [BookService, BookResolver]
 })
 export class BookModule {}
