cc765f3826c9481a9d1468ded569604e623f8f82
Vineet <vineetgoyat1@gmail.com>
2026-08-07 18:51:27 +0530

    postgreSQL graphql

--- src/book/model/book.model.ts
+++ src/book/model/book.model.ts
@@ -2,3 +2,8 @@ import { Schema, SchemaFactory, Prop } from '@nestjs/mongoose';
 import { Document } from 'mongoose'; 
 import { ObjectType, Field, ID } from '@nestjs/graphql';
 
+@Schema()
+@ObjectType()
+export class Book extends Document {
+    
+}
