768582074aba73da1b9f76e69f00db7a63dbc2fa
Vineet <vineetgoyat1@gmail.com>
2026-08-07 19:37:59 +0530

    postgreSQL graphql

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Book } from './model/book.model';
+import { CreateBookInput } from './dto/create-book.input';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class BookService {
-    constructor(@InjectModel(Book.name) private bookModel: Model<Book>) {
+    constructor(@InjectModel(Book.name) private bookModel: Model<Book>) {}
 
+    async create(input: CreateBookInput) : Promise<Book> {
+        const createdBook = new this.bookModel(input);
+        return createdBook.save();
+    }
+
+    async findAll(): Promise<Book[]> {
+        return this.bookModel
     }
 }
