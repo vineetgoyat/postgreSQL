2de4fc391bf527e09eb6cec78f1011152d5cd9c2
Vineet <vineetgoyat1@gmail.com>
2026-08-07 19:13:42 +0530

    postgreSQL graphql

---
+++ src/book/dto/create-book.input.ts
@@ -0,0 +1,16 @@
+import { InputType, Field } from '@nestjs/graphql';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+@InputType()
+export class CreateBookInput {
+    @Field()
+    @IsNotEmpty()
+    @IsString()
+    'title': string;
+
+    @Field({ nullable: true})
+    @IsString()
+    'description'?: string;
+
+
+}
