fba28a235086296613fba8d1393ebd6b4a13aa97
Vineet <vineetgoyat1@gmail.com>
2026-08-08 15:49:05 +0530

    postgreSQL graphql

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = Number(process.env.PORT ?? 3000);
+  const host = process.env.HOST ?? '127.0.0.1';
+  await app.listen(port, host);
+  // eslint-disable-next-line no-console
+  console.log(`Server listening at http://${host}:${port}`);
 }
+
 bootstrap();
