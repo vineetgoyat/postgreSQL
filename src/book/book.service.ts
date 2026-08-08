77284f476792045893f9edfb1745c0f8817ad546
Vineet <vineetgoyat1@gmail.com>
2026-08-08 13:28:31 +0530

    postgreSQL graphql

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -16,4 +16,8 @@ export class BookService {
     async findAll(): Promise<Book[]> {
         return this.bookModel.find().exec();
     }
+
+    async findOne( id: string ): Promise<Book> {
+        const book = await this.bookModel.findById(id).exec();
+    }
 }
