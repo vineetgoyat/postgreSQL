57ff5ebfb0fa4a9271591dcda5147af4daab6821
Vineet <vineetgoyat1@gmail.com>
2026-08-08 14:28:33 +0530

    postgreSQL graphql

--- src/book/resolvers/book.resolver.ts
+++ src/book/resolvers/book.resolver.ts
@@ -1,4 +1,15 @@
 import { Resolver } from '@nestjs/graphql';
+import { BookService } from '../book.service';
+import { Query } from '@nestjs/graphql';
+import { Book } from '../model/book.model';
 
 @Resolver()
-export class BookResolver {}
+export class BookResolver {
+    constructor( private readonly bookService: BookService) {}
+
+
+    @Query( () => [Book], {name: 'getAllBooks'} )
+    async findAll() {
+        return this.bookService.findAll();
+    }
+}
