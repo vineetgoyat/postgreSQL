7e1be4cbd18a88f48cb7091a6a4f33d1032a0bbf
Vineet <vineetgoyat1@gmail.com>
2026-08-08 14:53:00 +0530

    postgreSQL graphql

--- src/book/resolvers/book.resolver.ts
+++ src/book/resolvers/book.resolver.ts
@@ -1,7 +1,9 @@
-import { Args, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { BookService } from '../book.service';
 import { Query } from '@nestjs/graphql';
 import { Book } from '../model/book.model';
+import { CreateBookInput } from '../dto/create-book.input';
+import { UpdateBookInput } from '../dto/update-book.input';
 
 @Resolver()
 export class BookResolver {
@@ -17,4 +19,15 @@ export class BookResolver {
     async findOne(@Args('id', {type: () => String}) id: string) {
         return this.bookService.findOne(id);
     }
+
+    @Mutation( () => Book )
+    async createBook(@Args('input') input: CreateBookInput) {
+        return this.bookService.create(input);
+    }
+
+    @Mutation( () => Book )
+    async updateBook(@Args('input') input: UpdateBookInput) {
+        return this.bookService.update(input);
+    }
+
 }
